b6849a1d2b9928ea1143e16533a16028d2ae2b8b
Lauri Lahti <lauri.lahti@pkcgroup.com>
2017-10-20 11:20:09 +0300

    added "ro" flag for mounting vbox iso image

--- debian_stretch_desktop.sh
+++ debian_stretch_desktop.sh
@@ -21,7 +21,7 @@ m-a prepare
 # Install virtualbox guest additions
 wget http://download.virtualbox.org/virtualbox/5.2.0/VBoxGuestAdditions_5.2.0.iso
 mkdir -p vbox_additions
-mount -o loop VBoxGuestAdditions_5.2.0.iso ./vbox_additions
+mount -o loop,ro VBoxGuestAdditions_5.2.0.iso ./vbox_additions
 sudo ./vbox_additions/VBoxLinuxAdditions.run
 rm -rf VBoxGuestAdditions_5.2.0.iso
 umount ./vbox_additions
