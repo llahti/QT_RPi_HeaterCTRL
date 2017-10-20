28f5f8954cc0a2d5c80207e3bb6e4c2393e54e3e
Lauri Lahti <lauri.lahti@pkcgroup.com>
2017-10-20 19:32:26 +0300

    Fixing incorrect user and group of ssh authorized_keys. Added ssh server installation

--- debian_stretch_desktop.sh
+++ debian_stretch_desktop.sh
@@ -8,9 +8,11 @@ chmod 0440 /etc/sudoers.d/vagrant
 # Set insecure keypair for SSH so that vagrant can take SSH connection to this box
 mkdir -p ~/.ssh
 chmod 0700 ~/.ssh
+chown vagrant:vagrant ~/.ssh
 rm -rf ~/.ssh/authorized_keys
-wget https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub -O ~/.ssh/authorized_keys
+wget -q https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub -O ~/.ssh/authorized_keys
 chmod 0600 ~/.ssh/authorized_keys
+chown vagrant:vagrant ~/.ssh/authorized_keys
 
 # Install dependencies for building virtualbox guest additions
 apt-get update -y
@@ -19,13 +21,18 @@ apt-get install -y linux-headers-$(uname -r) build-essential module-assistant dk
 m-a prepare
 
 # Install virtualbox guest additions
-wget http://download.virtualbox.org/virtualbox/5.2.0/VBoxGuestAdditions_5.2.0.iso
+wget -q http://download.virtualbox.org/virtualbox/5.2.0/VBoxGuestAdditions_5.2.0.iso
 mkdir -p vbox_additions
 mount -o loop,ro VBoxGuestAdditions_5.2.0.iso ./vbox_additions
-sudo ./vbox_additions/VBoxLinuxAdditions.run
+./vbox_additions/VBoxLinuxAdditions.run
 rm -rf VBoxGuestAdditions_5.2.0.iso
 umount ./vbox_additions
 rm -rf vbox_additions
+
+# Install needed software
+apt-get install openssh-server
+
+# Uninstall junk to make box smaller
 
 # Restart the machine in order to use virtualbox kernel modules
 reboot
