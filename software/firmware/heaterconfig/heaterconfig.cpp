8fb536c18636d65b9559af75b4c301940e068a2c
Lauri Lahti <lauri.lahti@outlook.com>
2017-11-09 19:58:02 +0200

    Arranged config strings bit beter way

--- software/firmware/heaterconfig/heaterconfig.cpp
+++ software/firmware/heaterconfig/heaterconfig.cpp
@@ -9,23 +9,26 @@ void HeaterConfig::resetFactoryDefaults()
 {
   QSettings settings(COMPANYNAME, APPNAME);
   // Circulation Pump settings
-  settings.setValue(QString("CirculationPump/UsePump"), true);  // Defines whether controller is using pump
+  // *************************
+  settings.setValue("CirculationPump/AutoControl", true);  // Defines whether controller is turned on/off automatically
   settings.setValue("CirculationPump/StartTemperature", 80.0);  // Pump starting temperature
   settings.setValue("CirculationPump/StopTemperature", 75.0);  // Pump stop temperature
 
-  // Exhaust Fan
-  // Control method for exthaust fan.  Possibilities are: "switch", "pid"
-  settings.setValue("ExtFan/ControlMethod", "switch");
-  settings.setValue("ExtFan/MinSpeed", 20.0);
-  settings.setValue("ExtFan/PreferredSpeed", 50.0);
-  settings.setValue("ExtFan/MaxSpeed", 80.0);
-  settings.setValue("ExtFan/StartSpeed", 65.0);
-  settings.setValue("ExtFan/StartDuration", 1200);  // Start procedure duration in seconds
-  settings.setValue("ExtFan/FireOnTemp", 100.0);  // If ext.gas temp is above this then fire is on
-
   // Boiler
-  settings.setValue("Boiler/MaxTemp", 95.0);  // Maximum allowed water temperature in boiler. If this is exeeded then turn off fan
+  // ******
+  settings.setValue("Boiler/MaxTemp", 97.0);  // Maximum allowed water temperature in boiler. If this is exeeded then turn off fan
   settings.setValue("Boiler/TargetTemp", 90.0);
+  settings.setValue("Boiler/FireOffTemp", 80.0);  // When ext.gas temp is below this during to running it should be considered that the fire is off
+
+  // Control method for exthaust fan.  Possibilities are: "switch", "pid"
+  settings.setValue("Boiler/ControlMethod", "onoff");
+
+  // Start Procedure settings
+  settings.setValue("Boiler/Start/Duration", 1200);  // Start procedure duration in seconds
+  settings.setValue("Boiler/Start/FireOnTemp", 100.0);  // If ext.gas temp is above this then fire is on
+  settings.setValue("Boiler/Start/ExtFanSpeed", 65.0);
+
+  //
 
   return;
 }
