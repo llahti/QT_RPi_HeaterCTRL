ae1cdb2aadead176dd7dfc4002bcddbbe92d1621
Lauri Lahti <lauri.lahti@outlook.com>
2017-11-18 19:05:07 +0200

    WIP: main controller

--- software/firmware/controllers/controller_main.cpp
+++ software/firmware/controllers/controller_main.cpp
@@ -3,5 +3,13 @@
 Controller_Main::Controller_Main(QObject *parent)
     : Controller_Interface(parent)
 {
+  // Connect HAL signals to controllers
+  connect(hal, SIGNAL(measuredBoilerTemp(MeasurementPackage)),
+          circPump, SLOT(ProcessMeasurement(MeasurementPackage)));
 
+
+  // Setup HAL and Start HAL updates with 1 second periods
+  hal.setHWType("hal_dummy");
+  hal.init();
+  hal.startUpdates(1.0);
 }
--- software/firmware/controllers/controller_main.h
+++ software/firmware/controllers/controller_main.h
@@ -5,10 +5,21 @@
 
 #include "controller_interface.h"
 
+#include "controller_circulationpump.h"
+#include "hal.h"
+
 class Controller_Main : public Controller_Interface
 {
 public:
     Controller_Main(QObject* parent = Q_NULLPTR);
+
+    // Sub controllers as public members for easier access
+    Controller_CirculationPump circPump;
+
+    // HAL instance
+    HAL hal;
+
+private:
 };
 
 #endif // CONTROLLER_MAIN_H
